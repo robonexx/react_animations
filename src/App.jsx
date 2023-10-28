fc6b0bd9ad4bd0b99cb15b07e428cedbdf53a559
robonexx <robertwagar@gmail.com>
2023-10-28 09:37:59 +0200

    loader done with anime js

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css';
+import Loader from './components/loaders/Loader';
 
 function App() {
 
   return (
     <main>
-      
+      <Loader />
     </main>
   );
 }
--- src/components/loaders/Loader.jsx
+++ src/components/loaders/Loader.jsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import { useCallback, useState, useEffect, useRef } from 'react';
 import './loader.scss';
+import anime from 'animejs';
 
 const Loader = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const loaderRef = useRef(null);
+
+  const Animate = useCallback(() => {
+    const timeline = anime.timeline({
+      targets: loaderRef.current,
+      duration: 1500,
+      easing: 'easeOutSine',
+    });
+    timeline.add({
+      targets: loaderRef.current,
+      opacity: [0, 1],
+      width: ['0', '20vw'],
+      duration: 600,
+      delay: 100,
+      innerHTML: [0, 100 + '%'],
+      round: 10,
+    });
+  }, []);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {}, 1500);
+    Animate();
+    return () => clearTimeout(timeout);
+  }, [Animate]);
+
   return (
-    <div className='loader'>
+    <div className='loader' ref={loaderRef}>
       <span>0</span>
     </div>
   );
